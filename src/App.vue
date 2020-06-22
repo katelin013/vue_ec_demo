4edc7328599a31e99fa78180e8d4ce897a65d406
kayle <kayle@nextlink.com.tw>
2020-06-22 17:33:14 +0800

    add_vant

--- src/App.vue
+++ src/App.vue
@@ -6,22 +6,46 @@
     <!-- 內容 -->
     <router-view></router-view>
 
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
     <!-- 底部菜單 -->
-    <TabbarView></TabbarView>
+    <!-- <TabbarView></TabbarView> -->
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search" dot>标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o" badge="5">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" badge="20">标签</van-tabbar-item>
+    </van-tabbar>
+
 
   </div>
 </template>
 
 <script>
 import NavView from './components/NavView/NavView.vue'
-import TabbarView from './components/TabbarView/TabbarView.vue'
+// import TabbarView from './components/TabbarView/TabbarView.vue'
+import { Tabbar, TabbarItem } from 'vant';
+import { Button } from 'vant';
+
 
 export default {
   name: 'App',
   components: {
     NavView,
-    TabbarView
-  }
+    // TabbarView,
+    [Button.name]:Button,
+    [Tabbar.name]:Tabbar,
+    [TabbarItem.name]:TabbarItem
+  },
+  data() {
+    return {
+      active: 0,
+    };
+  },
 }
 </script>
 
