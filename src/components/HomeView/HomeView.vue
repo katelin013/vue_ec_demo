145e61e6de7e21354926d23b651018b07b3385b5
kayle <kayle@nextlink.com.tw>
2020-06-23 10:41:25 +0800

    add_swipe_banner

--- src/components/HomeView/HomeView.vue
+++ src/components/HomeView/HomeView.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="home-view">首頁</div>
+  <div class="home-view">
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img v-lazy="image" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import { Swipe, SwipeItem, Lazyload} from 'vant';
+Vue.use(Lazyload);
+
 export default {
+
   name: 'homeView',
+  components:{
+    [Swipe.name]:Swipe,
+    [SwipeItem.name]:SwipeItem,
+
+  },
   data(){
     return{
       title: '首頁',
-      navVieShow: true
+      navVieShow: true,
+      images: [
+        'https://img.yzcdn.cn/vant/apple-1.jpg',
+        'https://img.yzcdn.cn/vant/apple-2.jpg',
+        'https://img.yzcdn.cn/vant/apple-3.jpg',
+        'https://img.yzcdn.cn/vant/apple-4.jpg',
+        'https://img.yzcdn.cn/vant/apple-5.jpg',
+      ],
     }
   },
   created(){
@@ -18,6 +41,16 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.my-swipe .van-swipe-item {
+    color: #fff;
+    height: 13rem;
+    font-size: 1rem;
+    text-align: center;
+    background-color: lightgray;
+    img{
+      height: 13rem;
+      width: 100%;
+    }
+  }
 </style>
